ec3f9cf9f5be859ae1e7826c760509905bcf379c
Nishanth v <nishanthnavneeth200@gmail.com>
2022-08-17 21:56:06 +0530

    checking the working nothing to lookin

--- server_programe.cpp
+++ server_programe.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //your code begins here add comment lines befor each section and explain any complex logic if u r using
 
-//primaray base class for creating database for employees
+//primaray base class for creating database for employees inherit this class for further needs
 
 class employee_data {
     private:
