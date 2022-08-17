c47c98fe0fffacfffa7ebc670de4e7b5aba4130a
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2022-08-17 21:53:58 +0530

    blank commit

--- server_programe.cpp
+++ server_programe.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 //your code begins here add comment lines befor each section and explain any complex logic if u r using
+
+//primaray base class for creating database for employees
+
 class employee_data {
     private:
 
