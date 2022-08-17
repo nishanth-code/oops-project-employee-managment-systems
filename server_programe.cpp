3aedd4ca3fa0e500f8eccc7190bcece3dc8db965
Nishanth v <nishanthnavneeth200@gmail.com>
2022-08-17 20:33:23 +0530

    added required files

---
+++ server_programe.cpp
@@ -0,0 +1,8 @@
+//header file section
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+using namespace std;
+
+//your code begins here add comment lines befor each section and explain any complex logic if u r using
