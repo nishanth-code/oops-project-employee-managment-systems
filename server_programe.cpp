8d69ee7966278a7d3bf89fb56123480c25b73ab0
Nishanth v <nishanthnavneeth200@gmail.com>
2022-08-29 19:48:25 +0530

    basic class defination done functions yet to be programed

--- server_programe.cpp
+++ server_programe.cpp
@@ -5,12 +5,37 @@
 #include<stdlib.h>
 #include<math.h>
 using namespace std;
+struct date_
+{
+    int day;
+    int month;
+    int year;
+};
 
 //your code begins here add comment lines befor each section and explain any complex logic if u r using
 
 //primaray base class for creating database for employees inherit this class for further needs
 
-class employee_data {
-    private:
+class read_display//genral class for reading and displaying info may be we can replace it with operator overloading also
+{
+    protected:
+    virtual void read() =0;
+    virtual void display() =0;
+    
 
- }
+};
+
+class data_base: public read_display//database class inherithing read
+{
+     private:
+         static int employee_code;
+         char name[20];
+         char posting[5];
+         struct date_ d_o_b;
+         struct date_ d_o_j;
+         char qualification[10];
+         float salary;
+         float attendance;
+
+};
+
